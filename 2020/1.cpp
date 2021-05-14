a12c197e2cf77380e447f52492dc3ed62dfe88c2
unknown <61902921+mrmotta@users.noreply.github.com>
2021-05-14 09:39:05 +0200

    2020 - day 1 - a few other changes

--- 2020/1.cpp
+++ 2020/1.cpp
@@ -31,7 +31,8 @@ int main (int argc, char *argv[]) {
 
 	// Preparing the environment for the execution
 
-	int64_t results[2] = {0};
+	int64_t result[2] = {0};
+	bool foundResult[] = {false, false};
 	vector<int> list;			// The list of all the numbers read from the file
 	int tmpNumber;				// Only a temporary value used when reading
 
@@ -45,30 +46,29 @@ int main (int argc, char *argv[]) {
 	
 	cout << "Processing input..." << endl;
 
-	// Bruteforce method: checking every possible combination of two (order doesn't matter) untill I find the right one
+	// Bruteforce method: checking every possible combination of two and three (order doesn't matter) untill I find the right one
 
-	for (int index1 = 0; index1 < list.size(); index1 ++)
-		for (int index2 = index1 + 1; index2 < list.size(); index2 ++)
-			if (list[index1] + list[index2] == 2020) {
-				results[0] = list[index1] * list[index2];
-				break;
+	for (int index1 = 0; index1 < list.size(); ++ index1)
+		for (int index2 = index1 + 1; index2 < list.size(); ++ index2) {
+			if (!foundResult[0] && list[index1] + list[index2] == 2020) {
+				result[0] = list[index1] * list[index2];
+				foundResult[0] = true;
 			}
+			if (!foundResult[1])
+				for (int index3 = index2 + 1; index3 < list.size(); ++ index3)
+					if (list[index1] + list[index2] + list[index3] == 2020) {
+						result[1] = list[index1] * list[index2] * list[index3];
+						foundResult[1] = true;
+					}
+			if (foundResult[0] && foundResult[1])
+				goto print;
+		}
 
+	print:
 	cout << endl;
 	cout << "Results:" << endl;
-	cout << "- Part 1: " << results[0] << endl;
-
-	// Bruteforce method: checking every possible combination of three (order doesn't matter) untill I find the right one
-	
-	for (int index1 = 0; index1 < list.size(); index1 ++)
-		for (int index2 = index1 + 1; index2 < list.size(); index2 ++)
-			for (int index3 = index2 + 1; index3 < list.size(); index3 ++)
-				if (list[index1] + list[index2] + list[index3] == 2020) {
-					results[1] = list[index1] * list[index2] * list[index3];
-					break;
-				}
-
-	cout << "- Part 2: " << results[1] << endl << endl;
+	cout << "- Part 1: " << result[0] << endl;
+	cout << "- Part 2: " << result[1] << endl << endl;
 
 	return 0;
 }
